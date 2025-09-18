a5304cfa7fe45e5d9004c4c5824bbebf0a3b1323
Xuân Nhi <embe@example.com>
2025-09-18 15:48:39 +0700

    fix: add python-docx

--- app.py
+++ app.py
@@ -1051,9 +1051,9 @@ def lock_quiz(quiz_id):
 
 
 ########
-if __name__ == "__main__":
-    app.run(debug=True) 
 # if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port)
+#     app.run(debug=True) 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
