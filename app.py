26792d35d34afdb1793b55587ade015d76cf21e6
Xuân Nhi <embe@example.com>
2025-11-04 09:39:58 +0700

    xuat

--- app.py
+++ app.py
@@ -1520,10 +1520,10 @@ def ai_generate_exam_schedule(image_file):
     except Exception as e:
         print(f"Lỗi xử lý ảnh lịch thi: {e}")
         return None
-########
-# if __name__ == "__main__":
-#      app.run(debug=True) 
+#######
+#if __name__ == "__main__":
+#     app.run(debug=True) 
 if __name__ == "__main__":
-  port = int(os.environ.get("PORT", 5000))
-  app.run(host="0.0.0.0", port=port)
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host="0.0.0.0", port=port)
 
